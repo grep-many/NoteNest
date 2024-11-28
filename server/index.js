913199f4d8ea013ab402c633851b77568a345406
manish <me.manishkumaryadav@gmail.com>
2024-11-28 19:46:56 +0530

    reverted to index.js previous cors

--- server/index.js
+++ server/index.js
@@ -2,29 +2,11 @@ const cors = require('cors');
 const express = require('express');
 const connectDB = require('./db');
 const { port } = require('./config');
-
 const app = express();
 
 connectDB();
-
-// Allow only one origin
-const allowedOrigin = 'https://grep-many.github.io/NoteNest/';
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (origin === allowedOrigin || !origin) {
-        // Allow requests from the allowed origin or server-side requests (no origin for internal use)
-        callback(null, true);
-      } else {
-        // Reject other origins
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow all HTTP methods
-    credentials: true, // Allow cookies or authentication headers if needed
-  })
-);
-
+// Enable CORS for all methods and origins
+app.use(cors());
 app.use(express.json());
 
 // Available routes
